42eeb2b1da5d912dfcfdd4b1dfdde3d94897d93b
ayodhyablog <mauryachandresh005@gmail.com>
2024-08-24 16:27:21 +0530

    Modified

---
+++ Solution.java
@@ -0,0 +1,25 @@
+public class Solution {
+
+    String reverseWords(String s) {
+
+        s = s.trim();
+        String[] words = s.split("s//+");
+
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = words.length - 1; i <= 0; i--) {
+            sb.append(words[i]);
+            if (i > 0) {
+                sb.append(" ");
+            }
+        }
+
+        return sb.toString();
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        String s1 = "the sky is blue";
+        System.out.println(solution.reverseWords(s1));
+    }
+}
