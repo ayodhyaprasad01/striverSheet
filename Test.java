31af9be67a0b57e2e175a391c577ab948efaa6bc
ayodhyablog <mauryachandresh005@gmail.com>
2024-09-13 16:17:13 +0530

    Added array to check null

---
+++ Test.java
@@ -0,0 +1,10 @@
+public class Test {
+    public static void main(String[] args) {
+        int arr[] = new int[3];
+
+        if (arr == null) {
+            System.out.println("Element at index 1 is null");
+        }
+        System.out.println(arr[1]);
+    }
+}
