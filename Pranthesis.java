6c2a51edec13aa31dd980a753edcee767925ec53
ayodhyablog <mauryachandresh005@gmail.com>
2024-09-16 10:59:31 +0530

    CheckParanthesis

---
+++ Pranthesis.java
@@ -0,0 +1,22 @@
+import java.util.Stack;
+
+public class Pranthesis {
+    public static void main(String[] args) {
+        String str = "(([}))";
+        Stack<Character> st = new Stack<>();
+        int count=0;
+
+        for (char ch : str.toCharArray()) {
+
+            if(ch=='(' || ch=='{' || ch=='['  ){
+                st.push(ch);
+            }
+
+            else if (ch==')' || ch=='}' || ch==']' && !st.isEmpty()) {
+                char c=st.pop();
+                count++;
+            }
+        }
+            System.out.println(count);
+    }
+}
