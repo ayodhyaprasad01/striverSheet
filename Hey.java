6ef88f1375e461d1857ad6227e277a9e9040d885
ayodhyablog <mauryachandresh005@gmail.com>
2024-09-20 10:19:23 +0530

    swap two number without third variable

--- Hey.java
+++ Hey.java
@@ -1,5 +1,11 @@
 public class Hey {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+
+        int a = 3;
+        int b = 9;
+        a = a + b;
+        b = a - b;
+
     }
 }
