2221b60fa698192fc9c33babfaaea22211f112a0
ayodhyablog <mauryachandresh005@gmail.com>
2024-10-03 16:09:22 +0530

    Moore dot class file

---
+++ RankTransformaer.java
@@ -0,0 +1,30 @@
+public class RankTransformaer {
+
+    public int[] arrayRankTransform(int[] arr) {
+
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = 0; j < arr.length - 1 - i; j++) {
+
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+
+                }
+            }
+        }
+        return arr;
+
+    }
+
+    public static void main(String[] args) {
+
+        RankTransformaer rankTransformaer = new RankTransformaer();
+        int[] arr = { 40, 10, 20, 30 };
+        int[] result = rankTransformaer.arrayRankTransform(arr);
+
+        for (int i : result) {
+            System.out.println(i);
+        }
+    }
+}
