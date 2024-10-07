341d2597bf6b28363df6e97f66e017a714f46336
ayodhyablog <mauryachandresh005@gmail.com>
2024-10-08 00:07:48 +0530

    StringBuilderPractices

--- Mapp.java
+++ Mapp.java
@@ -35,6 +35,54 @@ class Mapp {
         Mapp mapp = new Mapp();
         System.out.println(mapp.checkInclusion("ab", "eidbaooo")); // true
         System.out.println(mapp.checkInclusion("ab", "eidboaoo")); // false
+        int a = 3, b = 4;
+        // System.out.println(a || b);
+        for (int i = 0; (i < a) || (i < b); i++) {
+            String s = "hello hey";
+            String[] arg = s.split(" ");
+            System.out.println(arg[1]);
+        }
 
+        // char ab = 'a';
+        // char ba = 'b';
+        // System.out.println(ab + ba);
+
+        // String s = "Hello the";
+        // StringBuilder sb = new StringBuilder(s);
+
+        // System.out.println((sb.charAt(0) + sb.charAt(1)));
+
+        // StringBuilder stb = new StringBuilder();
+        char ch1 = 't';
+        char ch2 = 'c';
+        // String sts = stb.append(ch1).append(ch2).toString();
+        // System.out.println(sts.equals("tc"));
+
+        // StringBuilder sb = new StringBuilder("Hello World");
+        // int length = sb.length();
+
+        // System.out.println(sb);
+        // sb.delete(0, 1);
+
+        // System.out.println(sb);
+
+        // String sts = new String("AB");
+        String sts="AB";
+
+        if (sts == "AB" || sts == "CD") {
+
+            System.out.println("True");
+        } else {
+            System.out.println("False");
+        }
+
+        // StringBuilder stb = new StringBuilder();
+        // String sts = stb.append(ch1).append(ch2).toString();
+
+        // if (sts == "AB" || sts == "CD") {
+        // System.out.println("true");
+        // } else {
+        // System.out.println("Fal");
+        // }
     }
 }
