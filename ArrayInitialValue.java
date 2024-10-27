7f68b41512afc19726ccd9aebf35ec6b8dc63364
AyodhyaPrasad <101314031+ayodhyaprasad01@users.noreply.github.com>
2024-10-27 19:49:40 +0530

    ArrayInitialValue

---
+++ ArrayInitialValue.java
@@ -0,0 +1,10 @@
+public class ArrayInitialValue {
+
+    public static void main(String[] args) {
+        int[] array = new int[5];
+
+        for (int i = 0; i < array.length; i++) {
+            System.out.println(array[i]);
+        }
+    }
+}
