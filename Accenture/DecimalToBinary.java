fcab1a3664e51666de11622ced96bcff3030337f
AyodhyaPrasad <101314031+ayodhyaprasad01@users.noreply.github.com>
2024-12-13 11:06:09 +0530

    converting Binary,octal,hex into decimal and vise vers

---
+++ Accenture/DecimalToBinary.java
@@ -0,0 +1,10 @@
+public class DecimalToBinary {
+    public static void main(String[] args) {
+
+        int decimal = 9;
+
+        String binary = Integer.toOctalString(decimal);
+        String binary = Integer.to
+        System.out.println(binary);
+    }
+}
