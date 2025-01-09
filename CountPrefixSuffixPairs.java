1590b272675e5d3456a53ad7d3d95f8e655b2664
AyodhyaPrasad <101314031+ayodhyaprasad01@users.noreply.github.com>
2025-01-09 18:42:47 +0530

    Modified CountPrefixAndSuffix class

--- CountPrefixSuffixPairs.java
+++ CountPrefixSuffixPairs.java
@@ -1,30 +1,8 @@
 class CountPrefixSuffixPairs {
-
-    public int countPrefixSuffixPairs(String[] words) {
-
-        int n = words.length;
-        int count = 0;
-
-        for (int i = 0; i < n; i++) {
-            for (int j = i + 1; j < n; j++) {
-
-                if (isPrefixAndSuffix(words[i], words[j])) {
-                    count++;
-                }
-            }
-        }
-        return count;
-    }
-
-    public boolean isPrefixAndSuffix(String str1, String str2) {
-        return str2.startsWith(str1) && str2.endsWith(str1);
-    }
-
     public static void main(String[] args) {
-        CountPrefixSuffixPairs obj = new CountPrefixSuffixPairs();
-        String[] words = { "abc", "def", "abc", "def", "abc",
-                "def", "abc", "def", "abc", "def" };
-        System.out.println("Number of prefix and suffix pairs: " + obj.countPrefixSuffixPairs(words));
+        String s = "abaccd";
+        String s1 = "ab";
+        System.out.println(s1.contains(s));
 
     }
 
