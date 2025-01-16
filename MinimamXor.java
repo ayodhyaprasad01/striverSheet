f88ac4924bda35ab90a496d925335af174563456
AyodhyaPrasad <101314031+ayodhyaprasad01@users.noreply.github.com>
2025-01-16 11:51:34 +0530

    Modified the MinimamXor.java and  MinimamXor.class

--- MinimamXor.java
+++ MinimamXor.java
@@ -1,37 +1,38 @@
 public class MinimamXor {
-    public int minimizeXor(int num1, int num2) {
-        int tarB = Integer.bitCount(num2);
-        int curB = Integer.bitCount(num1);
+    // public int minimizeXor(int num1, int num2) {
+    // int tarB = Integer.bitCount(num2);
+    // int curB = Integer.bitCount(num1);
 
-        if (curB == tarB) {
-            return num1;
-        }
+    // if (curB == tarB) {
+    // return num1;
+    // }
 
-        int result = num1;
+    // int result = num1;
 
-        if (curB > tarB) {
-            for (int i = 0; i < 32 && curB > tarB; i++) {
-                if ((result & (1 << i)) != 0) {
-                    result &= ~(1 << i);
-                    curB--;
-                }
-            }
-        } else {
-            for (int i = 0; i < 32 && curB < tarB; i++) {
-                if ((result & (1 << i)) == 0) {
-                    result |= (1 << i);
-                    curB++;
-                }
-            }
-        }
+    // if (curB > tarB) {
+    // for (int i = 0; i < 32 && curB > tarB; i++) {
+    // if ((result & (1 << i)) != 0) {
+    // result &= ~(1 << i);
+    // curB--;
+    // }
+    // }
+    // } else {
+    // for (int i = 0; i < 32 && curB < tarB; i++) {
+    // if ((result & (1 << i)) == 0) {
+    // result |= (1 << i);
+    // curB++;
+    // }
+    // }
+    // }
 
-        return result;
-    }
+    // return result;
+    // }
 
     public static void main(String[] args) {
-        MinimamXor m = new MinimamXor();
-        System.out.println(m.minimizeXor(5, 3)); // 3
-        System.out.println(m.minimizeXor(10, 7)); // 11
+        // MinimamXor m = new MinimamXor();
+        // System.out.println(m.minimizeXor(5, 3)); // 3
+        // System.out.println(m.minimizeXor(10, 7)); // 11
+        System.out.println(10 ^ 4); // 6
 
     }
 }
