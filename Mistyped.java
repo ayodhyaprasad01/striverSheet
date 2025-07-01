a9dbe805d1f2984a397f167b7c34ae483aab1347
AyodhyaPrasad <101314031+ayodhyaprasad01@users.noreply.github.com>
2025-07-01 10:03:57 +0530

    Adding MisMached Solution

---
+++ Mistyped.java
@@ -0,0 +1,29 @@
+public class Mistyped {
+    public int possibleStringCount(String word) {
+        int n = word.length();
+        int i = 0;
+        int count = 1;
+
+        while (i < n) {
+            int j = i;
+            while (j < n && word.charAt(j) == word.charAt(i)) {
+                j++;
+            }
+            int llen = j - i;
+            if (llen > 1) {
+                count += llen - 1;
+            }
+            i = j;
+        }
+        return count;
+    }
+
+    public static void main(String[] args) {
+        Mistyped mistyped = new Mistyped();
+        String word = "aaabbc";
+        int result = mistyped.possibleStringCount(word);
+        System.out.println("Possible string count for '" + word + "': " + result);
+        // This is a placeholder for the Mistyped class.
+        // The original code snippet does not provide any functionality or methods.
+    }
+}
